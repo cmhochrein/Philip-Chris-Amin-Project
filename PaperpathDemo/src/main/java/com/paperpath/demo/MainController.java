39981a1dcdc529e164698f3afb454ff8c2513369
unknown <cmhochrein@uncg.edu>
2023-12-04 22:23:29 -0500

    adding login get method

--- PaperpathDemo/src/main/java/com/paperpath/demo/MainController.java
+++ PaperpathDemo/src/main/java/com/paperpath/demo/MainController.java
@@ -1,11 +1,6 @@
 package com.paperpath.demo;
 
-import com.paperpath.demo.article_request.EditorController;
-import com.paperpath.demo.photos.PhotoController;
-import com.paperpath.demo.writer.WriterController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -14,8 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class MainController {
+
     @GetMapping(value = {"", "/", "/home"})
-    public String dashboard(Model model) {
+    public String getHome() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
     }
 }
