1663c1c3585958b833e6a1f9948692244f579e83
Philip Sijerkovic <pdsijerkovi@uncg.edu>
2023-12-05 00:44:34 -0500

    Index page glow up

--- PaperpathDemo/src/main/java/com/paperpath/demo/PaperPathApplication.java
+++ PaperpathDemo/src/main/java/com/paperpath/demo/PaperPathApplication.java
@@ -3,6 +3,7 @@ package com.paperpath.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PaperPathApplication {
 	public static void main(String[] args) {
